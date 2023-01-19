82f84a8def067eec1c4dc20d408cd4be8cd827e0
chawi rojas <nadaro@outlook.com>
2023-01-19 13:36:13 -0300

    slice

--- src/redux/tweetSlice.tsx
+++ src/redux/tweetSlice.tsx
@@ -1,5 +1,4 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { call, put, takeEvery } from "redux-saga/effects";//-->G6
 import { Res, IState, Posts } from "./types";
 
 const initialState: IState = { data: [], loading: false };
@@ -12,37 +11,6 @@ export const getPosts = createAsyncThunk("tweets/getPosts", async () => {
 
   return posts;
 });
-// function* fetchSaga(){
-//   try{}catch(error){}-->G7
-// }
-
-function* fetchTweets() {
-
-//  const rest = yield call(()=>)-->G8
-
-// yield put(addTweetFetch(posts));-->G9
-
-  try {
-    const res: Res = yield call(() =>
-      fetch("https://jsonplaceholder.typicode.com/posts")
-    );
-    const data: Posts[] = yield res.json();
-    const posts = data.map((post) => post.body);
-
-    yield put(addTweetFetch(posts));
-  } catch (error) {
-    console.log("The error", error);
-  }
-}
-
-
-export  function* fetchSaga() {
-  yield takeEvery("tweets/getPosts", fetchTweets);
-}
-
-// export function* fetchSaga(){ -->G10
-//   yield takeEvery("", );
-// }
 
 const tweetSlice = createSlice({
   name: "tweets",
@@ -54,10 +22,6 @@ const tweetSlice = createSlice({
     getPosts: (state) => {
       state.loading = true;
     },
-    // addTweetFetch: (state, action: PayloadAction<string[]>) => {
-    //   state.data.push(...action.payload);
-    //   state.loading = false;
-    // },
   },
   extraReducers: (builder) => {
     builder.addCase(
